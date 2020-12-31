056b275bb722bda90b613dade3ea35651aa3c9f2
JimHeisenberg <JimHeisenberg@hotmail.com>
2020-12-31 22:06:00 +0800

    add script for start frontend

--- server/run.sh
+++ server/run.sh
@@ -2,6 +2,9 @@
 # chmod +x run.sh
 # ./run.sh
 
+# start frontend
+# pm2 start npm -- start
+
 nohup python server.py > server.out 2>&1 &
 nohup python backend.py > backend.out 2>&1 &
 # nohup gunicorn -b 172.17.0.13:12345 backend:app > backend.out 2>&1 &
